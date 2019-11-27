b5688a5504c9aed5acc11ff9e36a67ae589dd899
小麻 <851417245@qq.com>
2019-11-27 21:13:08 +0800

    修改管理员删除功能的错误，添加表单上传时间的转换

--- src/main/java/com/lost_found/pojo/Post.java
+++ src/main/java/com/lost_found/pojo/Post.java
@@ -1,7 +1,9 @@
 package com.lost_found.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -17,6 +19,8 @@ public class Post {
     private String name;
 
     @ApiModelProperty(value = "失物拾物时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date loseTime;
 
     @ApiModelProperty(value = "失物拾物地点")

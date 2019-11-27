c0b38b86efa9fd68aa1cc1ffec387d55f9554a46
1347006947@qq.com <1347006947@qq.com>
2019-11-27 21:43:10 +0800

    模糊查询, 上传图片更新

---
+++ src/main/java/com/lost_found/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.lost_found.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Value("${upload.picture.path}")
+    private  String uploadPicturePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //其他静态资源
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/img/**").addResourceLocations("file:"+uploadPicturePath);
+    }
+}
